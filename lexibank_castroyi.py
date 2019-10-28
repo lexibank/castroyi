4bc651920ff6bed0615db6f311e60fc5fd5c8de5
lingulist <mattis.list@lingpy.org>
2019-10-28 13:04:52 +0100

    update

--- lexibank_castroyi.py
+++ lexibank_castroyi.py
@@ -1,22 +1,24 @@
-from __future__ import unicode_literals, print_function
+
+
+
+
+from pathlib import Path
+from pylexibank.dataset import Dataset as BaseDataset 
+from pylexibank import Concept, Language
+from pylexibank.util import pb
+
 from collections import OrderedDict, defaultdict
 
-import attr
-from clldutils.misc import slug
-from clldutils.path import Path
-from clldutils.text import split_text, strip_brackets
-from pylexibank.dataset import Dataset as BaseDataset 
-from pylexibank.dataset import Concept, Language
-
 from lingpy import *
-from tqdm import tqdm
+from clldutils.misc import slug
+import attr
 
 @attr.s
-class HConcept(Concept):
+class CustomConcept(Concept):
     Chinese_Gloss = attr.ib(default=None)
 
 @attr.s
-class HLanguage(Language):
+class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     SubGroup = attr.ib(default="Nesu")
@@ -26,43 +28,37 @@ class HLanguage(Language):
 class Dataset(BaseDataset):
     id = 'castroyi'
     dir = Path(__file__).parent
-    concept_class = HConcept
-    language_class = HLanguage
-    
-    def cmd_download(self, **kw):
-        pass
+    concept_class = CustomConcept
+    language_class = CustomLanguage
 
-    def cmd_install(self, **kw):
+    def cmd_makecldf(self, args):
 
-        wl = Wordlist(self.dir.joinpath('raw', 'yi-wl.tsv').as_posix())
-        langs = {} # need for checking later
-        concepts = {}
-
-        with self.cldf as ds:
-
-            for concept in self.conceptlist.concepts.values():
-                ds.add_concept(
-                        ID=concept.id,
-                        Name=concept.english,
-                        Chinese_Gloss=concept.attributes['chinese'],
-                        Concepticon_ID=concept.concepticon_id,
-                        Concepticon_Gloss=concept.concepticon_gloss
-                        )
-                concepts[concept.english] = concept.id
-            concepts['Daughter-in-law'] = concepts['daughter-in-law']
-            langs = {k['Name']: k['ID'] for k in self.languages}
-            ds.add_languages()
-            ds.add_sources(*self.raw.read_bib())
-            
-            for idx in tqdm(wl, desc='cldfify'):
-                 ds.add_segments(
-                    Language_ID=langs[wl[idx, 'doculect']],
-                    Parameter_ID=concepts[wl[idx, 'concept']],
-                    Value=wl[idx, 'value'],
-                    Form= wl[idx, 'form'],
-                    Segments=wl[idx, 'tokens'],
-                    Source=['Castro2010']
+        wl = Wordlist(self.raw_dir.joinpath('yi-wl.tsv').as_posix())
+        args.writer.add_sources()
+        language_lookup = args.writer.add_languages(
+                lookup_factory='Name')
+        concept_lookup = {}
+        for concept in self.conceptlist.concepts.values():
+            idx = concept.id.split('-')[-1]+'_'+slug(concept.english)
+            args.writer.add_concept(
+                    ID=idx,
+                    Name=concept.english,
+                    Concepticon_ID=concept.concepticon_id,
+                    Concepticon_Gloss=concept.concepticon_gloss,
+                    Chinese_Gloss = concept.attributes['chinese']
                     )
+            concept_lookup[concept.english] = idx
+        concept_lookup['Daughter-in-law'] = concept_lookup['daughter-in-law']
+        
+        for idx in pb(wl, desc='cldfify', total=len(wl)):
+             args.writer.add_form_with_segments(
+                Language_ID=language_lookup[wl[idx, 'doculect']],
+                Parameter_ID=concept_lookup[wl[idx, 'concept']],
+                Value=wl[idx, 'value'],
+                Form= wl[idx, 'form'],
+                Segments=wl[idx, 'tokens'],
+                Source=['Castro2010']
+                )
 
 
 
