d9c42bf5274de5a572017cedc0c315959b269b4c
chrzyki <christoph@functor-argument.net>
2021-07-21 10:00:19 +0200

    Release prep

--- lexibank_castroyi.py
+++ lexibank_castroyi.py
@@ -1,65 +1,54 @@
-
-
-
-
 from pathlib import Path
-from pylexibank.dataset import Dataset as BaseDataset 
-from pylexibank import Concept, Language
-from pylexibank.util import pb
 
-from collections import OrderedDict, defaultdict
-
-from lingpy import *
-from clldutils.misc import slug
 import attr
+from clldutils.misc import slug
+import lingpy
+import pylexibank
 
 
 @attr.s
-class CustomConcept(Concept):
+class CustomConcept(pylexibank.Concept):
     Chinese_Gloss = attr.ib(default=None)
 
 
 @attr.s
-class CustomLanguage(Language):
+class CustomLanguage(pylexibank.Language):
     SubGroup = attr.ib(default="Nesu")
     Family = attr.ib(default="Sino-Tibetan")
 
 
-class Dataset(BaseDataset):
-    id = 'castroyi'
+class Dataset(pylexibank.Dataset):
+    id = "castroyi"
     dir = Path(__file__).parent
     concept_class = CustomConcept
     language_class = CustomLanguage
 
     def cmd_makecldf(self, args):
 
-        wl = Wordlist(self.raw_dir.joinpath('yi-wl.tsv').as_posix())
+        wl = lingpy.Wordlist(self.raw_dir.joinpath("yi-wl.tsv").as_posix())
         args.writer.add_sources()
-        
-        languages = args.writer.add_languages(lookup_factory='Name')
-        
+
+        languages = args.writer.add_languages(lookup_factory="Name")
+
         concepts = {}
         for concept in self.conceptlists[0].concepts.values():
-            idx = concept.id.split('-')[-1] + '_' + slug(concept.english)
+            idx = concept.id.split("-")[-1] + "_" + slug(concept.english)
             args.writer.add_concept(
-                    ID=idx,
-                    Name=concept.english,
-                    Concepticon_ID=concept.concepticon_id,
-                    Concepticon_Gloss=concept.concepticon_gloss,
-                    Chinese_Gloss = concept.attributes['chinese']
-                    )
+                ID=idx,
+                Name=concept.english,
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss,
+                Chinese_Gloss=concept.attributes["chinese"],
+            )
             concepts[concept.english] = idx
-        concepts['Daughter-in-law'] = concepts['daughter-in-law']
-        
-        for idx in pb(wl, desc='cldfify', total=len(wl)):
-             args.writer.add_form_with_segments(
-                Language_ID=languages[wl[idx, 'doculect']],
-                Parameter_ID=concepts[wl[idx, 'concept']],
-                Value=wl[idx, 'value'],
-                Form= wl[idx, 'form'],
-                Segments=wl[idx, 'tokens'],
-                Source=['Castro2010']
-                )
+        concepts["Daughter-in-law"] = concepts["daughter-in-law"]
 
-
-
+        for idx in pylexibank.progressbar(wl, desc="cldfify", total=len(wl)):
+            args.writer.add_form_with_segments(
+                Language_ID=languages[wl[idx, "doculect"]],
+                Parameter_ID=concepts[wl[idx, "concept"]],
+                Value=wl[idx, "value"],
+                Form=wl[idx, "form"],
+                Segments=wl[idx, "tokens"],
+                Source=["Castro2010"],
+            )
--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
 from setuptools import setup
-import sys
 import json
 
 
-with open('metadata.json', encoding='utf-8') as fp:
+with open("metadata.json", encoding="utf-8") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_castroyi',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_castroyi'],
+    name="lexibank_castroyi",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_castroyi"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'castroyi=lexibank_castroyi:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=3.0',
-    ]
+    entry_points={"lexibank.dataset": ["castroyi=lexibank_castroyi:Dataset"]},
+    install_requires=["pylexibank>=3.0"],
 )
